f064180ced724caa675e0117ada96e439829aec3
Taylor Archibald <36053181+Tahlor@users.noreply.github.com>
2026-07-19 22:38:21 -0600

    Enable crop coverage safeguards

--- src/image_clustering/cropping/__init__.py
+++ src/image_clustering/cropping/__init__.py
@@ -3,6 +3,9 @@
 from .api import crop_clustering_result, crop_directory
 from .config import Config as CropConfig
 from .config import load_config, load_default_config
+from .coverage import install_coverage_guards
+
+install_coverage_guards()
 
 __all__ = [
     "CropConfig",
